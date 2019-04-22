2ef0fe58b065350ce8a9cd00b9cb04737adb5139
Joseph T. Parsons <josephtparsons@gmail.com>
2019-04-22 10:48:49 -0500

    Change Scanner to support all types of white-space.

--- src/RR/Shunt/Scanner.php
+++ src/RR/Shunt/Scanner.php
@@ -37,7 +37,7 @@ use RR\Shunt\Exception\SyntaxError;
 class Scanner
 {
     //              operator_________________________________|number_______________|word____________________|space_
-    const PATTERN = '/^([<>]=|<>|><|[!,><=&\|\+\-\*\/\^%\(\)]|\d*\.\d+|\d+\.\d*|\d+|[a-z_A-Zπ]+[a-z_A-Z0-9]*|[ \t]+)/';
+    const PATTERN = '/^([<>]=|<>|><|[!,><=&\|\+\-\*\/\^%\(\)]|\d*\.\d+|\d+\.\d*|\d+|[a-z_A-Zπ]+[a-z_A-Z0-9]*|\s+)/';
 
     const ERR_EMPTY = 'nothing found! (endless loop) near: `%s`';
     const ERR_MATCH = 'syntax error near `%s`';
